19be22f9e405abb0501ce7fe1107a6d5ecb6ecc0
pablofddz <74065131+pablofddz@users.noreply.github.com>
2024-07-10 19:00:43 +0200

    Update cuentaIBM.py

--- cuentaIBM.py
+++ cuentaIBM.py
@@ -2,7 +2,7 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 
 QiskitRuntimeService.save_account(
     channel="ibm_quantum",
-    token="5d799f890604e93d97df6f667a44d946c1edbe9c604e559b89711faf593eb0c1895286d9a32f0bd19a25d1c29e854504fedf27941d797cfc3d009df7e1e9471c",
+    token="",
     set_as_default=True,
     # Use `overwrite=True` if you're updating your token.
     overwrite=True,
