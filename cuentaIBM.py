47e21a3afbc9dab73af1bd85b81802a7b1c3313e
Pablo Fernandez <pablo.fernandezalegre@estudiante.uam.es>
2024-09-05 00:55:45 +0200

    Spanish -> English

--- cuentaIBM.py
+++ cuentaIBM.py
@@ -2,7 +2,7 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 
 QiskitRuntimeService.save_account(
     channel="ibm_quantum",
-    token="5d156b588e1f2a8a4ce8b95174acd58fac04edafb1337dcb39ca849c69abf1507f7a69d094518a2e0ecb030bc56456f2d6c1015330394b4eee98ea9f820c7a58",
+    token="",
     set_as_default=True,
     # Use `overwrite=True` if you're updating your token.
     overwrite=True,
